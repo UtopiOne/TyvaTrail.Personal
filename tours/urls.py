430f29a66192b05fc44e63a9d04575260067368c
KamilGasanov <gasanovkamil516@gmail.com>
2025-12-10 12:52:05 +0300

    add URLs and basic templates for home, profile and routes

---
+++ tours/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path("", views.home, name="home"),
+    path("profile/", views.profile_view, name="profile"),
+    path("my-routes/", views.my_routes, name="my_routes"),
+    path("routes/<int:pk>/", views.route_detail, name="route_detail"),
+]
