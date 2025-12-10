1733ad650621761de395f9f12c8a3fc109dac30b
KamilGasanov <gasanovkamil516@gmail.com>
2025-12-10 08:41:52 +0300

    add auto creation of UserProfile via signals

--- tours/apps.py
+++ tours/apps.py
@@ -2,4 +2,8 @@ from django.apps import AppConfig
 
 
 class ToursConfig(AppConfig):
-    name = 'tours'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "tours"
+
+    def ready(self):
+        from . import signals
